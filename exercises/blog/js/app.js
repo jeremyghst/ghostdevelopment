e521a6ac2b9db0c050d917be0b006ee59ee91e04
Jeremy Ghost <51990006+jeremyghst@users.noreply.github.com>
2021-07-08 21:23:35 +0200

    scale blog

--- exercises/blog/js/app.js
+++ exercises/blog/js/app.js
@@ -1,47 +1,31 @@
 "use strict"
 
-const body = document.body;
 
 const dateP = document.getElementById("dateP");
 const postArticle = document.getElementById("postArticle");
 const posts = document.getElementsByClassName("post");
-const module = document.getElementById("module");
 
 const postBtn = document.getElementById("postBtn");
-const closeBtn = document.getElementById("closeBtn");
-const updateBtn = document.getElementById("updateBtn");
-
 const searchBarInput = document.getElementById("searchBarInput");
-const messageArray = [];
 
 postBtn.addEventListener("click", createMessage);
-closeBtn.addEventListener("click", closeModule);
-updateBtn.addEventListener("click", updatePost);
-
 searchBarInput.addEventListener("input", search);
 
 function setDate(){
     const today = new Date();
-    const date = ((today.getDate() < 10) ? "0" + today.getDate() : today.getDate())+'-'+((today.getMonth()+1 < 10) ? "0" + (today.getMonth()+1) : today.getMonth()+1)+'-'+today.getFullYear();
+    const date = today.getDate()+'-'+(today.getMonth()+1)+'-'+today.getFullYear();
     const time = ((today.getHours() < 10) ? "0" + today.getHours() : today.getHours()) + ":" + ((today.getMinutes() < 10) ? "0" + today.getMinutes() : today.getMinutes()) + ":" + ((today.getSeconds() < 10) ? "0" + today.getSeconds() : today.getSeconds());
     const dateTime = date+' '+time;
     dateP.innerText = dateTime;
-
-    return dateTime;
 }
 setInterval(setDate, 1000);
 
 function createMessage(){
-    if(document.getElementById("messageTitle").value !== "" && document.getElementById("message").value !== ""){
-
     const message = new Message;
-    messageArray.push(message.create());
-    message.show(messageArray);
-    }
-    return;
+    message.create();
+
+    return false;
 }
-
-
 
 function search(){
     Array.from(posts).forEach(post => {
@@ -51,32 +35,4 @@ function search(){
             post.style.display = "block"
         }
     })
-}
-
-function showUpdatePost(){
-    module.classList.add("visible");
-    body.classList.add("body_visible");
- 
-    document.documentElement.scrollTop = 0;
-    document.getElementById("moduleTitle").value = this.getElementsByTagName("h2")[0].innerText;
-    document.getElementById("moduleMessage").value = this.getElementsByTagName("p")[0].innerText;
-    document.getElementById("moduleId").value = this.querySelector("input[type='hidden']").value;
-
-    return;
-}
-
-function updatePost(){
-    messageArray.forEach(message => {
-        if(message.id == this.parentNode.querySelector("input[type='hidden']").value){
-            message = message.update();
-        }
-    })
-    closeModule();
-}
-
-function closeModule(){
-    module.classList.remove("visible");
-    body.classList.remove("body_visible");
-
-    return;
 }
--- exercises/blog/js/class.js
+++ exercises/blog/js/class.js
@@ -1,95 +1,58 @@
 "use strict"
 
-let i = 0;
-
 class Message{
-    constructor(id, title, message, date, i){
-        this.id = id;
+    constructor(title, message, date){
         this.title = title;
         this.message = message;
         this.date = date;
     }
 
     create(){
+        if(document.getElementById("messageTitle").value !== "" && document.getElementById("message").value !== ""){
             const today = new Date();
-            const date = ((today.getDate() < 10) ? "0" + today.getDate() : today.getDate())+'-'+((today.getMonth()+1 < 10) ? "0" + (today.getMonth()+1) : today.getMonth()+1)+'-'+today.getFullYear();
-            const time = ((today.getHours() < 10) ? "0" + today.getHours() : today.getHours()) + ":" + ((today.getMinutes() < 10) ? "0" + today.getMinutes() : today.getMinutes()) + ":" + ((today.getSeconds() < 10) ? "0" + today.getSeconds() : today.getSeconds());
+            const date = today.getDate()+'-'+(today.getMonth()+1)+'-'+today.getFullYear();
+            const time = today.getHours() + ":" + today.getMinutes() + ":" + ((today.getSeconds() < 10) ? "0" + today.getSeconds() : today.getSeconds());
             const dateTime = date+' '+time;
 
             let messageTitle = document.getElementById("messageTitle");
             let messageMessage = document.getElementById("message");
             const searchBar = document.getElementById("searchBarInput");
 
-            this.id = i;
             this.title = messageTitle.value;
             this.message = messageMessage.value;
             this.date = dateTime;
-            i++;
 
-            return this;
-    }
-    
-    show(){
-        postArticle.innerHTML = "";
+            const post = document.createElement("div");
+            post.classList.add("post");
 
-        messageArray.forEach(message => {
-        const post = document.createElement("div");
-        post.classList.add("post");
-        post.addEventListener("click", showUpdatePost)
+            const title = document.createElement("h2");
+            title.innerText = this.title;
 
-        const hiddenInput = document.createElement("input");
-        hiddenInput.value = message.id;
-        hiddenInput.type = "hidden";
+            const paragraph = document.createElement("p");
+            paragraph.innerText = this.message;
 
-        const title = document.createElement("h2");
-        title.innerText = message.title;
+            const small = document.createElement("small");
+            small.innerText = "Date: " + this.date;
 
-        const paragraph = document.createElement("p");
-        paragraph.innerText = message.message;
+            const deleteBtn = document.createElement("button");
+            deleteBtn.innerText = "X";
+            deleteBtn.classList.add("deleteBtn");
+            deleteBtn.addEventListener("click", this.delete);
 
-        const small = document.createElement("small");
-        small.innerText = "Date: " + message.date;
+            post.append(deleteBtn, title, paragraph, small);
 
-        const deleteBtn = document.createElement("button");
-        deleteBtn.innerText = "X";
-        deleteBtn.classList.add("deleteBtn");
-        deleteBtn.addEventListener("click", message.delete);
+            postArticle.insertBefore(post, postArticle.childNodes[0]);
 
-        post.append(hiddenInput, deleteBtn, title, paragraph, small);
-     
-        
-
-        searchBar.focus();
-        postArticle.insertBefore(post, postArticle.childNodes[0]);
-
-        })
-
-        return;
+            searchBar.focus();
+        } else {
+            return;
+        }
     }
 
-    update(){
-        this.title = document.getElementById("moduleTitle").value;
-        this.message = document.getElementById("moduleMessage").value;
-
-        this.show();
-    }
-
-    delete(event){
+    delete(){
         if(confirm("Are you sure you want to delete this message?")){
             const post = this.parentNode;
             postArticle.removeChild(post);
-
-            messageArray.forEach(message => {
-                if(message.id == post.querySelector("input[type='hidden']").value){
-                    const index = messageArray.indexOf(message);
-                    if (index > -1) {
-                        messageArray.splice(index, 1);
-                    }
-                }
-            })
-
         }
-        event.stopImmediatePropagation();
-        return;
     }
 }
