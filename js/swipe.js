cdc2c3fcea5418738a282a8c3cea3ad7a29b17e2
ghostdevelopment <ghostdevelopment@ghostdevelopment.ghostdevelopment.nl>
2021-11-16 23:34:51 +0100

    swipe

---
+++ js/swipe.js
@@ -0,0 +1,25 @@
+"use strict"
+
+window.addEventListener('touchstart', swipe_start);
+window.addEventListener('touchmove', swipe_move);
+
+window.addEventListener('touchend', swipe_end);
+
+let swipe_start_y;
+let swipe_end_y;
+
+function swipe_start(e){
+    swipe_start_y = e.touches[0].clientY;  
+    return swipe_start_y;
+}
+
+function swipe_move(e){
+    swipe_end_y = e.touches[0].clientY;
+    return swipe_end_y   
+}
+
+function swipe_end(){
+    if(swipe_start_y > swipe_end_y){
+        move_section('up');
+    }
+}
