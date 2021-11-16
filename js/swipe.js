f4d8558f85049c00e4b77657c1d5866ef8a1eda6
ghostdevelopment <ghostdevelopment@ghostdevelopment.ghostdevelopment.nl>
2021-11-17 00:17:36 +0100

    swipe

--- js/swipe.js
+++ js/swipe.js
@@ -6,25 +6,42 @@ window.addEventListener('touchmove', swipe_move);
 window.addEventListener('touchend', swipe_end);
 
 let swipe_start_y;
+let swipe_start_x;
+
 let swipe_end_y;
+let swipe_end_x;
 
 function swipe_start(e){
     swipe_start_y = e.touches[0].clientY;  
+    swipe_start_x = e.touches[0].clientX;  
+
     swipe_end_y = swipe_start_y;
-    return swipe_start_y;
+    swipe_end_x = swipe_start_x;
+    return;
 }
 
 function swipe_move(e){
     swipe_end_y = e.touches[0].clientY;
-    return swipe_end_y   
+    swipe_end_x = e.touches[0].clientX;
+
+    return;   
 }
 
 function swipe_end(){
-    if(swipe_start_y !== swipe_end_y){
+    console.log(swipe_start_y - swipe_end_y);
+    console.log(swipe_start_x - swipe_end_x);
+
+    if(Math.abs(swipe_start_y - swipe_end_y) > 25){
         if(swipe_start_y > swipe_end_y){
             move_section('down');
         } else {
             move_section('up');
+        }
+    } else if(swipe_start_x !== swipe_end_x){
+        if(swipe_start_x > swipe_end_x){
+            move_about_dots('left');
+        } else {
+            move_about_dots('right');
         }
     }
 }
