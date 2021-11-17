e6ad3cbae776e4a80c7f7a24eb64fd91a6854cd7
ghostdevelopment <ghostdevelopment@ghostdevelopment.ghostdevelopment.nl>
2021-11-17 14:34:34 +0100

    mobile use_portrait

--- js/device.js
+++ js/device.js
@@ -1,21 +1,49 @@
 "use strict"
 
-const device_elements = Array.from(document.getElementsByClassName('device'));
+let device; 
+const use_portrait = document.createElement('div');
+use_portrait.classList.add('use_portrait');
+const use_portrait_img = document.createElement('img');
+use_portrait_img.innerText = 'please rotate 90deg';
+use_portrait_img.src = 'assets/ghost/ghost.svg';
+use_portrait_img.classList.add('use_portrait_img');
+
+use_portrait.appendChild(use_portrait_img);
 
 function deviceType(){
     const ua = navigator.userAgent;
-    let device = 'desktop';
+    device = 'desktop';
     
     if (/(tablet|ipad|playbook|silk)|(android(?!.*mobi))/i.test(ua)) {
         device =  "tablet";
     }
     else if (/Mobile|Android|iP(hone|od)|IEMobile|BlackBerry|Kindle|Silk-Accelerated|(hpw|web)OS|Opera M(obi|ini)/.test(ua)) {
         device =  "mobile";
+        isLandscape();
     }
 
-    device_elements.forEach(element => {
-        element.classList.add(device);
-    })
+    console.log(device);
 };
-
 deviceType();
+
+if(device === 'mobile'){
+    window.addEventListener("orientationchange", function() {
+        isLandscape()
+    });
+}
+
+function isLandscape(){
+    const  screen_orientation = screen.orientation || screen.mozOrientation || screen.msOrientation;
+
+
+    if(screen_orientation.type === 'landscape-primary'){
+            body.appendChild(use_portrait);
+        return
+    } else {
+        if(document.getElementsByClassName('use_portrait')[0]){
+            body.removeChild(use_portrait);
+        }
+    }
+    
+   return;
+}
