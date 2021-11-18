59e44802a8ced00c376e28b9b8bf7a3882179afb
ghostdevelopment <ghostdevelopment@ghostdevelopment.ghostdevelopment.nl>
2021-11-18 04:06:48 +0100

    facebook

---
+++ js/facebook.js
@@ -0,0 +1,7 @@
+"use strict"
+
+const facebook_btn = document.getElementById('facebook');
+facebook_btn.addEventListener('click', () => {
+    setTimeout(function () { window.open("https://www.facebook.com/jeremy.ghst.5/", '_blank'); }, 25);
+    window.location = "fb://jeremy.ghst.5/";
+})
