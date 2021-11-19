4bf37b098de251c6c9655a24afcf81e7536fdaf1
ghostdevelopment <ghostdevelopment@ghostdevelopment.ghostdevelopment.nl>
2021-11-19 23:54:27 +0100

    facebook

--- js/facebook.js
+++ js/facebook.js
@@ -1,22 +1,9 @@
 "use strict"
 
 const facebook_btn = document.getElementById('facebook');
+facebook_btn.addEventListener('click', () => {
+    setTimeout(function () { window.open("https://www.facebook.com/jeremy.ghst.5/", '_blank'); }, 25);
+    window.location = "fb://profile/100039459462203/";
 
-facebook_btn.addEventListener('click', () => {    
-    try {
-        window.location.href = "fb://profile/100039459462203/";
-     } catch (e) {
-        console.log(e);
-     } finally {
-        console.log('finally')
-     }
+    event.stopPropagation;
 })
-
-// if(window.location = "fb://profile/100039459462203/"){
-//     console.log('IOS app')
-// } else if (window.location = "fb://page/100039459462203/"){
-//     console.log('Andriod app')
-// } else if (window.open("https://www.facebook.com/jeremy.ghst.5/", '_blank')){
-//     console.log('website');
-    
-// }
