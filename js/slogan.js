1afb489a14a36b8fc65c460a30357bcb76de1e05
ghostdevelopment <ghostdevelopment@ghostdevelopment.ghostdevelopment.nl>
2021-11-20 16:08:53 +0100

    peekaboo

--- js/slogan.js
+++ js/slogan.js
@@ -9,10 +9,15 @@ const slogan_container = document.getElementById('slogan');
 const slogan = ['The worst part about', 'having all those bugs', 'is,', 'people expect you', 'to behave as if', "you don't"];
 let slogan_part = slogan[i].split("");
 
+const peekaboo = document.getElementById('peekaboo');
+
 function type_slogan() {
         if(i < slogan.length){
             slogan_part = slogan[i].split("");
 
+            if(i === 2){
+                peekaboo.classList.add('boo');
+            }
             type_slogan_timeout = setTimeout(function () {
                 if(j < slogan_part.length){
                     if(j === 0){
@@ -38,8 +43,12 @@ function type_slogan() {
         } else{
             i = 0;
             j = 0;
+            peekaboo.classList.remove('boo');
             clearInterval(type_slogan_timeout);
             return;
         }
 }
-type_slogan();
+
+if(section === 2){
+    type_slogan();
+}
